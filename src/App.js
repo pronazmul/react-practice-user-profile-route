8ecd142325c05c19dbe210674343ac9f0247a50a
Nazmul Huda <mhnazmul48@gmail.com>
2020-08-23 20:24:00 +0600

    Conditional Return Using State value Check

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StatePractice from './components/StatePractice';
+import ConditionalReturnIfElse from './components/ConditionalReturnIfElse';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (  
-    <StatePractice></StatePractice>
+          <div>
+              <h1 className='text-center'>Conditional Return Using State value Check</h1>
+              <ConditionalReturnIfElse></ConditionalReturnIfElse>
+                <hr></hr>
+              <h1 className='text-center'>State value set and Update</h1>
+              <StatePractice></StatePractice>
+          </div>
    )
 }
 
