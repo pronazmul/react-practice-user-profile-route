3255e6b84dd3207277d382532b0115c267a30fc8
Nazmul Huda <mhnazmul48@gmail.com>
2020-08-23 22:59:50 +0600

    Automatic Refresh Using force Update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import StatePractice from './components/StatePractice';
 import ConditionalReturnIfElse from './components/ConditionalReturnIfElse';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import ConditionalReturnElementVarriable from './components/ConditionalReturnElementVarriable';
+import RefreshNow from './components/RefreshNow';
 
 function App() {
   return (  
           <div>
+              <h1 className='text-center'>Automaitce Force Update</h1>
+              <RefreshNow></RefreshNow>
+              <hr></hr>
               <h1 className='text-center'>Conditional Return inside return block</h1>
               <ConditionalReturnElementVarriable></ConditionalReturnElementVarriable>
               <hr></hr>
