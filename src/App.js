8517f05e4ab46bc8eb60df875fcd85eed5d9d90a
Nazmul Huda <mhnazmul48@gmail.com>
2020-08-23 20:57:04 +0600

    conditional return using element varriable like turnary operator inside return block

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 import StatePractice from './components/StatePractice';
 import ConditionalReturnIfElse from './components/ConditionalReturnIfElse';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ConditionalReturnElementVarriable from './components/ConditionalReturnElementVarriable';
 
 function App() {
   return (  
           <div>
+              <h1 className='text-center'>Conditional Return inside return block</h1>
+              <ConditionalReturnElementVarriable></ConditionalReturnElementVarriable>
+              <hr></hr>
               <h1 className='text-center'>Conditional Return Using State value Check</h1>
               <ConditionalReturnIfElse></ConditionalReturnIfElse>
                 <hr></hr>
